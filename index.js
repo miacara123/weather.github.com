cdd2a6e003d4040063faadf04be8566d37cade1d
miacara123 <miacara123.github.com>
2017-10-31 12:21:32 +0800

    weather
    weather

---
+++ index.js
@@ -0,0 +1,88 @@
+var china = new Object();
+china['北京市'] = [{name: '北京市区'}];
+china['上海市'] = [{name: '上海市区'}];
+china['天津市'] = [{name: '天津市区'}];
+china['重庆市'] = [{name: '重庆市区'}];
+china['黑龙江省'] = [{name: '哈尔滨市'}, {name: '齐齐哈尔市'}, {name: '大庆市'}, {name: '牡丹江市'}, {name: '佳木斯市'}, {name: '绥化市'}, {name: '黑河市'}, {name: '双鸭山市'}, {name: '鸡西市'}, {name: '伊春市'}, {name: '七台河市'}, {name: '鹤岗市'}, {name: '加格达奇市'}];
+china['吉林省'] = [{name: '长春市'}, {name: '吉林市'}, {name: '延边州'}, {name: '四平市'}, {name: '通化市'}, {name: '白城市'}, {name: '辽源市'}, {name: '松原市'}, {name: '白山市'}];
+china['辽宁省']=[{name: '沈阳市'},{name:'大连市'},{name:'鞍山市'},{name:'抚顺市'},{name:'本溪市'},{name:'丹东市'},{name:'锦州市'},{name:'营口市'},{name:'阜新市'},{name:'辽阳市'},{name:'铁岭市'},{name:'朝阳市'},{name:'盘锦市'},{name:'葫芦岛市'}];
+china['内蒙古自治区']={name:'呼和浩特市'},{name:'包头市'},{name:'乌海市'},{name:'集宁市'},{name:'通辽市'},{name:'赤峰市'},{name:'鄂尔多斯市'};
+china['河北省'] = [{name:'石家庄'},{name:'保市定'}, {name:'张家市口'},{name:'承德市'},{name:'唐山市'},{name:'廊坊市'},{name:'沧州市'}, {name:'衡水市'},{name:'邢台市'},{name:'邯郸市'}, {name:'秦皇市岛'}];
+china['山西省']=[{name:'太原市'},{name:'大同市'},{name:'阳泉市'},{name:'晋中市'},{name:'长治市'},{name:'晋城市'},{name:'临汾市'},{name:'运城市'},{name:'朔州市'},{name:'忻州市'},{name:'吕梁市'}];
+china['陕西省']=[{name:'西安市'},{name:'咸阳市'},{name:'延安市'},{name:'榆林市'},{name:'渭南市'},{name:'商洛市'},{name:'安康市'},{name:'汉中市'},{name:'宝鸡市'},{name:'铜川市'}];
+china['山东省']=[{name:'济南市'},{name:'青岛市'},{name:'淄博市'},{name:'德州市'},{name:'烟台市'},{name:'潍坊市'},{name:'济宁市'},{name:'泰安市'},{name:'临沂市'},{name:'菏泽市'},{name:'滨州市'},{name:'东营市'},{name:'威海市'},{name:'枣庄市'},{name:'日照市'},{name:'莱芜市'},{name:'聊城市'}];
+china['新疆维吾尔自治区']=[{name:'乌鲁木齐市'},{name:'克拉玛依市'}];
+china['西藏自治区']=[{name:'拉萨市'}];
+china['青海省']=[{name:'西宁市'}];
+china['甘肃省']=[{name:'兰州市'},{name:'定西市'},{name:'平凉市'},{name:'庆阳市'},{name:'武威市'},{name:'金昌市'},{name:'张掖市'},{name:'酒泉市'},{name:'天水市'},{name:'武都市'},{name:'临夏市'},{name:'合作市'},{name:'白银市'}];
+china['宁夏回族自治区']=[{name:'银川市'},{name:'石嘴山市'},{name:'吴忠市'},{name:'固原市'},{name:'中卫市'}];
+china['河南省']=[{name:'郑州市'},{name:'安阳市'},{name:'新乡市'},{name:'许昌市'},{name:'平顶山市'},{name:'信阳市'},{name:'南阳市'},{name:'开封市'},{name:'洛阳市'},{name:'商丘市'},{name:'焦作市'},{name:'鹤壁市'},{name:'濮阳市'},{name:'周口市'},{name:'漯河市'},{name:'驻马店市'},{name:'三门峡市'}];
+china['江苏省']=[{name:'南京市'},{name:'无锡市'},{name:'镇江市'},{name:'苏州市'},{name:'南通市'},{name:'扬州市'},{name:'盐城市'},{name:'徐州市'},{name:'淮安市'},{name:'连云港市'},{name:'常州市'},{name:'泰州市'},{name:'宿迁市'}];
+china['湖北省']=[{name:'武汉市'},{name:'襄樊市'},{name:'鄂州市'},{name:'孝感市'},{name:'黄冈市'},{name:'黄石市'},{name:'咸宁市'},{name:'荆州市'},{name:'宜昌市'},{name:'恩施市'},{name:'十堰市'},{name:'神农架'},{name:'随州市'},{name:'荆门市'}];
+china['浙江省']=[{name:'杭州市'},{name:'湖州市'},{name:'嘉兴市'},{name:'宁波市'},{name:'绍兴市'},{name:'台州市'},{name:'温州市'},{name:'丽水市'},{name:'金华市'},{name:'衢州市'},{name:'舟山市'}];
+china['安徽省']=[{name:'合肥市'},{name:'蚌埠市'},{name:'芜湖市'},{name:'淮南市'},{name:'马鞍山市'},{name:'安庆市'},{name:'宿州市'},{name:'阜阳市'},{name:'亳州市'},{name:'黄山市'},{name:'滁州市'},{name:'淮北市'},{name:'铜陵市'},{name:'宣城市'},{name:'六安市'},{name:'巢湖市'},{name:'池州市'}];
+china['福建省']=[{name:'福州市'},{name:'厦门市'},{name:'宁德市'},{name:'莆田市'},{name:'泉州市'},{name:'漳州市'},{name:'龙岩市'},{name:'三明市'},{name:'南平市'}];
+china['江西省']=[{name:'南昌市'},{name:'九江市'},{name:'上饶市'},{name:'抚州市'},{name:'宜春市'},{name:'吉安市'},{name:'赣州市'},{name:'景德镇市'},{name:'萍乡市'},{name:'新余市'},{name:'鹰潭市'}];
+china['湖南省']=[{name:'长沙市'},{name:'湘潭市'},{name:'株洲市'},{name:'衡阳市'},{name:'郴州市'},{name:'常德市'},{name:'益阳市'},{name:'娄底市'},{name:'邵阳市'},{name:'岳阳市'},{name:'张家界市'},{name:'怀化市'},{name:'黔阳市'},{name:'永州市'},{name:'吉首市'}];
+china['贵州省']=[{name:'贵阳市'},{name:'遵义市'},{name:'安顺市'},{name:'都匀市'},{name:'凯里市'},{name:'铜仁市'},{name:'毕节市'},{name:'六盘水市'},{name:'黔西市'}];
+china['四川省 ']=[{name:'成都市'},'攀枝花市',{name:'自贡市'},{name:'绵阳市'},{name:'南充市'},{name:'达州市'},{name:'遂宁市'},{name:'广安市'},{name:'巴中市'},{name:'泸州市'},{name:'宜宾市'},{name:'内江市'},{name:'资阳市'},{name:'乐山市'},{name:'眉山市'},{name:'凉山市'},{name:'雅安市'},{name:'甘孜市'},{name:'阿坝市'},{name:'德阳市'},{name:'广元市'}];
+china['广东省']=[{name:'广州市'},{name:'韶关市'},{name:'惠州市'},{name:'梅州市'},{name:'汕头市'},{name:'深圳市'},{name:'珠海市'},{name:'佛山市'},{name:'肇庆市'},{name:'湛江市'},{name:'江门市'},{name:'河源市'},{name:'清远市'},{name:'云浮市'},{name:'潮州市'},{name:'东莞市'},{name:'中山市'},{name:'阳江市'},{name:'揭阳市'},{name:'茂名市'},{name:'汕尾市'}];
+china['云南省']=[{name:'昆明市'},{name:'大理市'},{name:'红河'},{name:'曲靖市'},{name:'保山市'},{name:'文山市'},{name:'玉溪市'},{name:'楚雄'},{name:'普洱市'},{name:'昭通市'},{name:'临沧市'},{name:'怒江'},{name:'香格里拉'},{name:'丽江市'}];
+china['广西壮族自治区']=[{name:'南宁市'},{name:'崇左市'},{name:'柳州市'},{name:'来宾市'},{name:'桂林市'},{name:'梧州市'},{name:'贺州市'},{name:'贵港市'},{name:'玉林市'},{name:'百色市'},{name:'钦州市'},{name:'河池市'},{name:'北海市'},{name:'防城港市'}];
+china['海南省']=[{name:'海口'},{name:'三亚'}];
+china['香港特别行政区']=[{name:'香港'}];
+china['澳门特别行政区']=[{name:'澳门'}];
+china['台湾省'] = [{name:'台北市'},{name:'高雄市'},{name:'台南市'},{name:'台中市'},{name:'桃园市'},{name:'新竹市'},{name:'宜兰'},{name:'马公'},{name:'嘉义市'}];
+// china['江西省']=[{name:'南昌市',code:101240101},{name:'九江市',code:101240201}];
+function chinaChange(province, city) {
+    var pv, cv;
+    var i, ii;
+    var l, r, n;
+    pv = province.value;
+    cv = city.value;
+    city.length = 1;
+    if (pv == '0') return;
+    if (typeof (china[pv]) == 'undefined') return;
+
+    var arr = Object.keys(china);
+    var index = arr.indexOf(pv)+1;
+    console.log(arr.indexOf(pv)+1);
+        for (i = 0; i < china[pv].length; i++) {
+            // l=101nn
+            // r=nn01
+            ii = i + 1;
+
+            // console.log(j);
+            if(index<=4){
+                if (i < 9) {
+                    r = '0' + (i + 1) + '00';
+                } else {
+                    r = (i + 1) + '00';
+                }
+            }else{
+                if (i < 9) {
+                    r = '0' + (i + 1) + '01';
+                } else {
+                    r = (i + 1) + '01';
+                }
+            }
+
+
+            if (index < 10) {
+                l = '1010' + index;
+            } else {
+                l = '101' + index;
+            }
+            china[pv][i].code = l + r;
+            console.log(china[pv][i].code);
+            city.options[ii] = new Option();
+            city.options[ii].text = china[pv][i].name;
+            city.options[ii].value = china[pv][i].code;
+        }
+    // }
+    city.options[0].text = "请选择市区";
+
+    console.log(china[pv]);
+};
+
+
